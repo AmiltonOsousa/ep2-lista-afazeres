fb24430138f5d93fcda99f019b77dea50a0b40a1
Amilton Oliveira <amilton.o.sousa@gmail.com>
2026-06-14 12:14:05 -0300

    feat: adicionar reconhecimento de emails e url

--- main.rb
+++ main.rb
@@ -3,9 +3,23 @@ texto = gets.chomp
 
 tags = texto.scan(/#[a-zA-ZÀ-ÿ0-9_]+/)
 
+emails = texto.scan(
+  /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/
+)
+
+urls = texto.scan(
+  %r{https?:\/\/[^\s]+}i
+)
+
 puts "\n=== Resultado ==="
 
 puts "Tags:"
 tags.each do |tag|
   puts "- #{tag}"
 end
+
+puts "\nEmails:"
+emails.each { |email| puts "- #{email}" }
+
+puts "\nURLs:"
+urls.each { |url| puts "- #{url}" }
