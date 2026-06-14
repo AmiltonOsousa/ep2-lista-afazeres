6b8047f76ca02e6c635a351b2e38b299c5cc058e
Amilton Oliveira <amilton.o.sousa@gmail.com>
2026-06-14 12:20:19 -0300

    feat: implementar reconhecimento dos padroes de horario

--- main.rb
+++ main.rb
@@ -3,15 +3,19 @@ texto = gets.chomp
 
 tags = texto.scan(/#[a-zA-ZÀ-ÿ0-9_]+/)
 
-emails = texto.scan(
-  /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/
-)
+emails = texto.scan(/\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/)
 
-urls = texto.scan(
-  %r{https?:\/\/[^\s]+}i
-)
+urls = texto.scan(%r{https?:\/\/[^\s]+}i)
+
+horarios = []
+horarios += texto.scan(/\b(?:[01]?\d|2[0-3])\s+[0-5]\d\b/)
+horarios += texto.scan(/\b(?:[01]?\d|2[0-3])\s*horas?\b/i)
+horarios += texto.scan(/às\s+(?:[01]?\d|2[0-3])/i)
 
 puts "\n=== Resultado ==="
+
+puts "Horários:"
+horarios.each { |h| puts "- #{h}" }
 
 puts "Tags:"
 tags.each { |tag| puts "- #{tag}" }
